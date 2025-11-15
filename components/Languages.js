0af338eca57f3ef3043ab8acf7a702b630d0f0e6
madany <madanyo85@gmail.com>
2025-11-15 21:59:16 +0200

    Languages

--- components/Languages.js
+++ components/Languages.js
@@ -1,12 +1,82 @@
-import React from 'react'
-import { View } from 'react-native'
+import { StyleSheet, Text, View } from "react-native"
 
 function Languages() {
   return (
-    <View>
-        Languages
+    <View style={styles.main}>
+
+    <View style={styles.container}>
+
+      <Text style={styles.title}>Languages</Text>
+
+      <View style={styles.row}>
+        <Text style={styles.skillName}>ِArabic</Text>
+
+        <View style={styles.barBack}>
+          <View style={[styles.barFill, { width: "95%" }]} />
+        </View>
+      </View>
+
+      <View style={styles.row}>
+        <Text style={styles.skillName}>Engish</Text>
+
+        <View style={styles.barBack}>
+          <View style={[styles.barFill, { width: "82%" }]} />
+        </View>
+      </View>
+
+    </View>
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+     backgroundColor: "#2E2E2E",
+        padding: 15,
+        borderRadius: 10,
+        width: "90%",
+        alignSelf: "center",
+        marginTop: 20
+  
+  },
+
+  title: {
+    color: "#cccccc",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 15,
+    textAlign: "center"
+  },
+
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 15
+  },
+
+  skillName: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    width: "35%"
+  },
+
+  barBack: {
+    width: "65%",    
+    height: 8,
+    backgroundColor: "#444",
+    borderRadius: 4,
+  },
+
+  barFill: {
+    height: "100%",
+    backgroundColor: "#FFC107",
+    borderRadius: 4
+  },  
+  main: {
+    backgroundColor: "#1f1f1f",
+  }
+})
 
 export default Languages
